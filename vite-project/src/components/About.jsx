516fe452cbb6759f20d4ff901a6e87d3aa476d86
sdilolle <stephdilolle@gmail.com>
2024-08-01 18:22:20 -0400

    fixed issue where about me was cut off. re-pasted full paragraph

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -5,7 +5,9 @@ const About = () => {
     return (
         <section id="about">
             <h2>About Me</h2>
-            <p>After 10 years in the customer service industry, I have decided to make a career pivot into Coding and Web Development...</p>
+            <p>After 10 years in the customer service industry, I have decided to make a career pivot into Coding and Web Development. I enrolled in the Rutgers University/edX Full Stack Bootcamp, a 6 month intensive program in which we study both front-end and back-end development. In this course, I have gained skills with HTML, CSS, JavaScript, Node.js, Express.js, SQL, Third party APIs, Server-side APIs, and more.
+        
+            I have prior experience in project management and team leadership from my job as a Merchandising Team Leader (MTL), where I planned and mapped out each monthly floorset. After the planning phase, I had to delegate tasks and lead my team to get the entire store changed in a timely manner over the course of 2-3 days. I also led a team in shipment processing, and prior to my time as MTL, I led sales teams as a Selling Team Leader for several years.</p>
         </section>
     );
 };
